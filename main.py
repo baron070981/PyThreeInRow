187b24d209ca198a7f3fc7ed662b0a14234c3b1a
baron070981 <baron070981@gmail.com>
2025-03-10 21:55:56 +0300

    не большие изменения

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ from utils import *
 os.system("clear && clear")
 
 pg.init()
+clock = pg.time.Clock()
 
 WIDTH_WIN = 900
 HEIGHT_WIN = 760
 
+GS_WIDTH = WIDTH_WIN * 0.88
+GS_HEIGHT = HEIGHT_WIN * 0.69
+GS_Y = HEIGHT_WIN * 0.25
+GS_X = HEIGHT_WIN * 0.07
+
+bg_src = './src/background.png'
+
 screen = pg.display.set_mode((WIDTH_WIN, HEIGHT_WIN))
 pg.display.set_caption("Три в ряд")
-clock = pg.time.Clock()
 
-
-
+bg_space = Space(bg_src, WIDTH_WIN, HEIGHT_WIN)
+gamespace = GameSpace((0,0,0,0), GS_WIDTH, GS_HEIGHT, (GS_X, GS_Y))
 
 
 def main():
@@ -36,9 +43,11 @@ def main():
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
                 ...
 
-        screen.fill((255, 255, 255))
+        screen.fill((55, 55, 55))
+        screen.blit(bg_space.image, (0,0))
         
-        
+        gamespace.draw(screen)
+        gamespace.draw_rect(screen)
         
 
         pg.display.flip()
