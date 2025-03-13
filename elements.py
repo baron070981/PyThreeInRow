ecceaca87da1b27879d4b6ef10f2ec4ee4ca97e0
baron070981 <baron070981@gmail.com>
2025-03-13 11:23:20 +0300

    добавил метод анимации выбранного элемента

--- elements.py
+++ elements.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from random import choice
+import math
 
 import pygame as pg
 
@@ -21,12 +22,12 @@ class Element(pg.sprite.Sprite):
         self.image = self.original_image.copy()
         self.rect = self.image.get_rect(center=pos)
         self.pos = pos
-        self.state = False
-        self.angle = 0
-        self.direct_rotate = 1
-        self.MOVE_STATE = False
+        self.scale_k = 0
+        self.last_time = pg.time.get_ticks()
+        self.selected = False
     
     def equal(self, value):
+        """сравнение двух элементов по их именам"""
         if self.name is None or value.name is None:
             raise ValueError('Не определены имена в Element')
         return self.name == value.name
@@ -37,97 +38,43 @@ class Element(pg.sprite.Sprite):
     def __repr__(self):
         return self.name
     
-    def set_random_image(self, *sources:str|Path|tuple):
-        src = choice(sources)
-        self.set_image(src)
-    
     def draw_rect(self, screen):
-        pg.draw.rect(screen, (200, 50, 50), self.rect, width=2)
-    
-    def set_image(self, src):
-        if isinstance(src, (str, Path)):
-            self.original_image = pg.image.load(str(src)).convert_alpha()
-            self.original_image = pg.transform.scale(self.original_image, (self.width, self.height))
-        elif isinstance(src, tuple):
-            self.original_image = pg.Surface((self.width, self.height))
-            self.original_image.fill(src)
-        self.image = self.original_image.copy()
-        self.rect = self.image.get_rect(center=self.rect.center)
-    
-    def animated_select(self, step, angle):
-        if self.state:
-            if self.direct_rotate == 1 and self.angle < angle:
-                self.angle += step
-            if self.angle >= angle and self.direct_rotate == 1:
-                self.direct_rotate = -1
-                self.angle = angle
-            if self.direct_rotate == -1 and self.angle > -angle:
-                self.angle -= step
-            if self.direct_rotate == -1 and self.angle <= -angle:
-                self.direct_rotate = 1
-                self.angle = -angle
-            self.image = pg.transform.rotate(self.original_image, self.angle)
-            self.rect = self.image.get_rect()
-            self.rect.center = self.pos
-        else:
-            self.image = self.original_image.copy()
-            self.rect = self.image.get_rect(center=self.pos)
-            self.angle = 0
-            self.direct_rotate = 1
+        """вспомогательный метод для отрисовки границ self.rect"""
+        pg.draw.rect(screen, (250, 250, 50), self.rect, width=2)
 
+    def set_image(self, path:str|Path):
+        """установка изображения из файла"""
+        self.original_image = pg.image.load(str(path)).convert_alpha()
+        self.original_image = pg.transform.scale(self.original_image, (self.width, self.height))
+        self.image = self.original_image.copy()
+        self.rect = self.image.get_rect(center=self.pos)
+    
     def reset(self):
+        """сброс некоторых параметров и флагов до начального значения"""
         self.image = self.original_image.copy()
         self.rect = self.image.get_rect(center=self.pos)
-        self.angle = 0
-        self.direct_rotate = 1
-        self.state = False
+        self.selected = False
+        self.scale_k = 0
 
-    def update(self, *args, **kwargs):
-        return super().update(*args, **kwargs)
-    
-    def scale_focuse(self, scale, is_focuse=False):
-        if is_focuse:
-            w, h = self.width, self.height
-            self.image = pg.transform.scale(self.original_image, (w+6, h+6))
-        else:
-            self.image = self.original_image.copy()
-        self.rect = self.image.get_rect()
-        self.rect.center = self.pos
-    
-    def animated_move(self, target_pos):
-        step = 15
-        if self.MOVE_STATE:
-            while True:
-                tx, ty = target_pos
-                x, y = self.rect.center
-                if abs(x - tx) >= step:
-                    x += -step if x > tx else step if x < tx else 0
-                else:
-                    x = tx
-                if abs(y - ty) >= step:
-                    y += -step if y > ty else step if y < ty else 0
-                else:
-                    y = ty
-                self.image = self.original_image.copy()
-                self.rect = self.image.get_rect()
-                self.rect.center = (x, y)
-                self.pos = (x, y)
-                if x != tx or y != ty:
-                    yield self.rect.center, self
-                else:
-                    self.MOVE_STATE = False
-                    return
+    def animate_select(self, ticks=50):
+        """анимацмя выбранного элемента"""
+        if not self.selected: return
+        now_time = pg.time.get_ticks()
+        if now_time - self.last_time >= ticks:
+            self.last_time = now_time
+            w, h = self.original_image.get_size()
+            scale_vals = [0, -1, -2, -3, -4, -5, -5, -4, -3, -2, -1, 0]
+            index = self.scale_k % len(scale_vals)
+            self.scale_k += 1
+            if self.scale_k >= len(scale_vals): self.scale_k = 0
+            value = scale_vals[index]
+            w += value
+            h += value
+            self.image = pg.transform.scale(self.original_image, (w, h))
+            self.rect = self.image.get_rect(center=self.pos)
 
-    def animated_delete(self, step=1):
-        w, h =  self.image.get_size()
-        w -= step
-        # h -= step
-        if w <= step // 2 + 1: return False
-        self.image = pg.transform.scale(self.image, (w, h))
-        self.rect = self.image.get_rect(center=self.pos)
-        return True
-
-
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
 
 
