b6a51ab1f909e743819fce613657dfb9580ab2ca
baron070981 <baron070981@gmail.com>
2025-03-13 11:26:34 +0300

    ---

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from random import randint
 import os
+from pathlib import Path
 
 from elements import *
 from spaces import *
@@ -11,28 +12,53 @@ os.system("clear && clear")
 pg.init()
 clock = pg.time.Clock()
 
+FPS = 40
 WIDTH_WIN = 900
 HEIGHT_WIN = 760
 
 GS_WIDTH = WIDTH_WIN * 0.88
 GS_HEIGHT = HEIGHT_WIN * 0.69
 GS_Y = HEIGHT_WIN * 0.25
-GS_X = HEIGHT_WIN * 0.07
+GS_X = WIDTH_WIN * 0.07
 
-bg_src = './src/background.png'
+ROOT = Path(__file__).parent
+SRC = ROOT / 'src'
+SMILES = SRC / 'smiles'
+
+bg_src = SRC / 'background.png'
+radost = SMILES / 'radost.png'
+zzz = SMILES / 'zzz.png'
+nyam = SMILES / 'nyamnyam.png'
+hohot = SMILES / 'hohot.png'
+gnev = SMILES / 'gnev.png'
+balbes = SMILES / 'balbes.png'
+pechal = SMILES / 'pechal.png'
+krutoy = SMILES / 'krutoy.png'
+ulybka = SMILES / 'ulybka.png'
+udivlenie = SMILES / 'udivlenie.png'
+
+
+
+
 
 screen = pg.display.set_mode((WIDTH_WIN, HEIGHT_WIN))
 pg.display.set_caption("Три в ряд")
 
 bg_space = Space(bg_src, WIDTH_WIN, HEIGHT_WIN)
-gamespace = GameSpace((0,0,0,0), GS_WIDTH, GS_HEIGHT, (GS_X, GS_Y))
+gamespace = GameSpace((250, 250, 250, 50), GS_WIDTH, GS_HEIGHT, (GS_X, GS_Y))
 
+element_size = gamespace.get_element_size(2, 2, 5)
+top, left = gamespace.get_first_paddings(element_size, 5)
+top += GS_Y
+left += GS_X
+
+test_element = Element(balbes, element_size, element_size, (left, top))
 
 def main():
     running = True
+    animate = False
 
     while running:
-        clock.tick(30)
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -41,7 +67,12 @@ def main():
             
             # обработка нажатия левой кнопки мыши
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-                ...
+                mpos = pg.mouse.get_pos()
+                # if pg.mouse.get_focused():
+                #     ...
+                #     test_element.selected = not test_element.selected
+                #     if not test_element.selected:
+                #         test_element.reset()
 
         screen.fill((55, 55, 55))
         screen.blit(bg_space.image, (0,0))
@@ -49,10 +80,14 @@ def main():
         gamespace.draw(screen)
         gamespace.draw_rect(screen)
         
+        test_element.draw(screen)
+        test_element.draw_rect(screen)
+        test_element.animate_select(70)
+        
 
         pg.display.flip()
 
-        clock.tick(40)
+        clock.tick(FPS)
     pg.quit()
 
 
