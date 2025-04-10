0a464d92b53f82380800645572226950d5d03aa6
baron070981 <baron070981@gmail.com>
2025-04-10 19:09:29 +0300

    добавил вывод очков, ходов и рекордов.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from elements import *
 from spaces import *
 import utils
+from records import *
 
 os.system("clear && clear")
 
@@ -21,10 +22,15 @@ GS_HEIGHT = HEIGHT_WIN * 0.69
 GS_Y = HEIGHT_WIN * 0.25
 GS_X = WIDTH_WIN * 0.07
 
+TEXT_COLOR1 = (255, 255, 255)
+TEXT_COLOR2 = (255, 30, 30)
+GS_COLOR = (252, 252, 252, 110)
+
 ROOT = Path(__file__).parent
 SRC = ROOT / 'src'
 SMILES = SRC / 'smiles'
 ADL = SRC / 'pshik_animate'
+FONT_PTH = SRC / 'fonts'
 
 bg_src = SRC / 'background.png'
 radost = SMILES / 'radost.png'
@@ -63,10 +69,10 @@ screen = pg.display.set_mode((WIDTH_WIN, HEIGHT_WIN))
 pg.display.set_caption("Три в ряд")
 
 bg_space = Space(bg_src, WIDTH_WIN, HEIGHT_WIN)
-gamespace = GameSpace((150, 150, 50, 180), GS_WIDTH, GS_HEIGHT, (GS_X, GS_Y))
-
-rows = 6
-columns = 5
+gamespace = GameSpace(GS_COLOR, GS_WIDTH, GS_HEIGHT, (GS_X, GS_Y))
+# gamespace = GameSpace((150, 150, 50, 180), GS_WIDTH, GS_HEIGHT, (GS_X, GS_Y))
+rows = 8
+columns = 9
 pad = 15
 element_size = gamespace.get_element_size(rows, columns, pad)
 top, left = gamespace.get_first_paddings(element_size, pad)
@@ -83,7 +89,12 @@ def main():
     scores = 0 # очки
     record = 0 # рекорд очков за кол-во ходов
     limit = 30 # макс кол-во ходов
-    count_limit_steps = 0 # счетчик сделанных ходов
+    count_limit_steps = 30 # счетчик сделанных ходов
+    record = get_record()
+    RECORD = False
+    new_record_text = "Новый рекорд:  :-("
+
+    font = pg.font.Font(FONT_PTH / "DejaVuSans-Bold.ttf", 28)
 
     while running:
 
@@ -113,8 +124,7 @@ def main():
                                     el1.target_pos = el2.rect.center
                                     el2.target_pos = el1.rect.center
                                     gamespace.swap_elements_group.add(el1, el2)
-                                    count_limit_steps += 1
-                                    print("Count steps:", count_limit_steps)
+                                    count_limit_steps -= 1 if count_limit_steps > 0 else 0
                                 else:
                                     el2.selected = True
                                     gamespace.add_selected(el2)
@@ -153,9 +163,30 @@ def main():
 
 
         for el in gamespace.delete_elements_group:
-            scores += el.animate_delete(animate_delete_list, 50)
+            if count_limit_steps > 0:
+                scores += el.animate_delete(animate_delete_list, 50)
+            else:
+                el.animate_delete(animate_delete_list, 50)
+                if scores > record and not RECORD:
+                    RECORD = True
+                    record = scores
+                    write_record(record)
+        
+
+        if count_limit_steps == 0 and RECORD:
+            new_record_text = f"Новый рекорд:  {record}"
 
         gamespace.el_group.draw(screen)
+
+        score_surface = font.render(f"Очки: {scores}", True, TEXT_COLOR1)
+        color = TEXT_COLOR1 if count_limit_steps > 5 else TEXT_COLOR2
+        step_surface = font.render(f"Ходы: {count_limit_steps}", True, color)
+        last_record_surface = font.render(f"Старый рекорд: {record}", True, TEXT_COLOR1)
+        new_record_surface = font.render(new_record_text, True, TEXT_COLOR1)
+        screen.blit(score_surface, (50, 33))
+        screen.blit(step_surface, (300, 33))
+        screen.blit(last_record_surface, (540, 18))
+        screen.blit(new_record_surface, (540, 50))
 
         pg.display.flip()
 
